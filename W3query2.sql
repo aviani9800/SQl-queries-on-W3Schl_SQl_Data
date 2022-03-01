461773bfee882b0dd8e998121a5d7eed62d6ec2e
Avinandan Dutta <75080175+aviani9800@users.noreply.github.com>
2022-03-01 13:13:28 +0530

    Create W3query2.sql

---
+++ W3query2.sql
@@ -0,0 +1,5 @@
+-- MAX,MIN & AVG 	Quantity Ordered.
+
+SELECT MAX(Quantity) AS MaxQuan FROM OrderDetails   -- 120
+SELECT AVG(Quantity) AS AvgQuan FROM OrderDetails   -- 24.6
+SELECT MIN(Quantity) AS MinQuan FROM OrderDetails   -- 01
